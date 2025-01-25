d702a2afcae570434f5f5e5efb97bd5037097b32
PabloC2A <116039498+PabloC2A@users.noreply.github.com>
2025-01-24 19:37:12 -0500

    Actualizacion Backend y Base de Datos con Notificaciones

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,6 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-const categoriaRoutes = require('./routes/categoria.routes');
-const rolRoutes = require('./routes/rol.routes');
-const tipoCrowdfundingRoutes = require('./routes/tipoCrowdfunding.routes');
-const userRoutes = require('./routes/usuario.routes');
-
 const app = express();
 
 // Middlewares globales
@@ -14,10 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 //Rutas
-app.use('/categoria', categoriaRoutes);
-app.use('/rol', rolRoutes);
-app.use('/tipoCrowdfunding', tipoCrowdfundingRoutes);
-app.use('/user', userRoutes);
 
 const PORT = process.env.PORT || 3301;
 app.listen(PORT, () => {
